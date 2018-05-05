f424193fb0a79879d1c46b2c21bde722824079bd
Kabir Goel <kabirgoel.kg@gmail.com>
2018-05-05 19:01:18 +0530

    Some changes

---
+++ public/javascripts/create-session.js
@@ -0,0 +1,5 @@
+$("#create-session").on("click", e => {
+  e.preventDefault()
+  console.log("create-session")
+  document.querySelector(".create").style.display = "block"
+})
--- public/scripts/create-session.js
+++
@@ -1,4 +0,0 @@
-$('#createSession').on('click', (e) => {
-    e.preventDefault();
-    document.querySelector('.create').style.display = 'block';
-})
